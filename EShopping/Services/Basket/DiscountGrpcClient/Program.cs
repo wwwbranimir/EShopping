9fe3ec8004c35c61df43bc5663997a08afa9f518
Branimir Bzenic <A-ECC1053@kraftheinz.com>
2024-05-17 22:02:22 +0200

    Fix string concatenation in Program.cs and update port mapping in docker-compose
    In Program.cs, the string concatenation method for printing the discount amount and description has been corrected from using a comma to using the plus operator. A `Console.ReadLine();` statement has also been added to pause the console until user input. In docker-compose.override.yml, the port mapping for the service has been updated from 8084:443 to 8085:443 due to potential port conflict.

--- EShopping/Services/Basket/DiscountGrpcClient/Program.cs
+++ EShopping/Services/Basket/DiscountGrpcClient/Program.cs
@@ -6,7 +6,8 @@ using Discount.Grpc.Protos;
 using var channel = GrpcChannel.ForAddress("http://localhost:8084");
 var discountProtoClient = new DiscountProtoService.DiscountProtoServiceClient(channel);
 var discount = await discountProtoClient.GetDiscountAsync(new GetDiscountRequest { ProductName = "Adidas Quick Force Indoor Badminton Shoes" });
-Console.WriteLine("Discount Amount:", discount.Amount);
-Console.WriteLine("Discount Description:", discount.Description);
+Console.WriteLine("Discount Amount: " + discount.Amount);
+Console.WriteLine("Discount Description: " + discount.Description);
+Console.ReadLine();
 
 
