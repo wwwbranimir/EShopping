80449ed0190cb4f4c37c9e1ea89303b5d27b8f6b
Branimir Bzenic <A-ECC1053@kraftheinz.com>
2024-06-20 14:28:09 +0200

    Added Ocelot.ApiGateway project and related configurations
    Updated the solution file `EShopping.sln` to include two new projects: `ApiGateways` and `Ocelot.ApiGateway`, with the latter nested under the former. Created `Ocelot.ApiGateway.csproj` with .NET 8.0 as the target framework and package references to `Ocelot` and `Ocelot.Cache.CacheManager`. Added two new JSON files `appsettings.Development.json` and `appsettings.json` with no content yet. Created `ocelot.Development.json` and `ocelot.Local.json` configuration files for routing configurations in different environments. Also, added `Program.cs` and `Startup.cs` files to the `Ocelot.ApiGateway` project for application configuration and service setup.

---
+++ EShopping/ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocelot.ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config.AddJsonFile("ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json"
+                       ,true,true);
+               }).ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+
+        }
+    }
+}
---
+++ EShopping/ApiGateways/Ocelot.ApiGateway/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.ApiGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { 
+
+            //services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ocelot!");
+                });
+            });
+            app.UseOcelot();
+        }
+    }
+}
